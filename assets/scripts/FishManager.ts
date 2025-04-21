63bff9ed788f83aa58de85208c9b042f7300b336
Ryan Chen <y.f.chenroderick@gmail.com>
2025-04-21 10:40:36 +0800

    chore: 修正命名

---
+++ assets/scripts/FishManager.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('FishManager')
+export class FishManager extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
